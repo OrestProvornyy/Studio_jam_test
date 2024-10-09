e70e239a5d8d608b25963f2f744a09e81aed3a76
OrestProvornyy <orestprovornyy@gmail.com>
2024-10-09 16:23:52 +0300

    lesson6

---
+++ lesson_7/lesson_7.py
@@ -0,0 +1,13 @@
+text = '\u0445\u0443\u0439\u043f\u0456\u0437\u0434\u0430\u0230'
+print(text)
+
+
+def greeting(first_name: str, second_name: str): -> str:
+""""""
+    return f'Hello {first_name.title()}, {second_name.title()}'
+
+
+
+greeting('orest', 'provornyy')
+greeting('nastinka', 'bulochkina')
+=
